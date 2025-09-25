a21ca04070dde8db6354dfb93fbbd493555c7a14
henriquetg1 <henriquetg1@al.insper.edu.br>
2025-09-25 12:01:14 -0300

    feat: add CORS middleware and configure allowed origins, users router included

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,30 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from .routers import users
 from . import models
 from .database import engine
 
 app = FastAPI()
 
+# CORS settings
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Create the database tables if they do not exist
 models.Base.metadata.create_all(bind=engine)
+
+# Include routers
+app.include_router(users.router)
 
 @app.get("/")
 def root():
