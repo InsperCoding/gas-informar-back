21e75db820b8d6c4c2651c7182bd8ae66039272b
ribollequis87 <gustavocr2@al.insper.edu.br>
2026-04-18 14:11:52 -0300

    feat: novo domínio vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,8 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://gas-informar-front.vercel.app"
+    "https://gas-informar-front.vercel.app",
+    "https://informar.vercel.app"
 ]
 
 app.add_middleware(
